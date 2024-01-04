85130853f5e58334ac2861e900052a4b93989b92
DavidDanielAlves <140220407+DavidDanielAlves@users.noreply.github.com>
2024-01-04 11:15:58 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ let palavras = [
 
     animais = ['gato', 'cachorro', 'galinha', 'elefante', 'cavalos', 'cobra', 'papagaio'],
 
-    objetos = ['cadeira', 'mesa', 'porta', 'janela', 'televisao', 'relogio', 'celular'],
+    objetos = ['cadeira', 'mesa', 'porta', 'janela', 'televisão', 'relógio', 'celular'],
 
-    comida = ['banana', 'maçã', 'alface', 'tomate', 'macarrão', 'feijao', 'arroz']
+    comida = ['banana', 'maçã', 'alface', 'tomate', 'macarrão', 'feijão', 'arroz']
 ]
 
 let numeroIndiceTipo = parseInt(Math.random() * 3);
@@ -21,16 +21,12 @@ let formulario = document.querySelector('.form');
 let mgsDeInstrucao = document.getElementById('msg-Instrucoes');
 let tentativasNohtml = document.getElementById('tentativas');
 let imgForca = document.querySelector('.container-forca img');
-let numeroDeTentativas = 8
+let numeroDeTentativas = 6;
 let letrasChutada = [];
 
-tentativasNohtml.innerHTML = numeroDeTentativas;
-
-quantidadeDeLetrasNoHtml.innerHTML = numeroDeLetras;
-
-tipo.innerHTML = tipoDaPalavra;
-
-palavraNoHtml.innerHTML = palavraEscondida;
+function adicionarAoHtml (elemento, valor) {
+    elemento.innerHTML = valor
+}
 
 function esconderPalavra(palavra) {
     let caracteres = palavra.split('');
@@ -54,9 +50,56 @@ function revelarCaracteres(palavra, valorDoInput) {
     palavraEscondida = palavraReveladaArray.join(' ');
 
     palavraNoHtml.innerHTML = palavraEscondida;
-
-    console.log('palavraEscondida:', palavraEscondida); // Adicione este log
 }
+
+function jogadorAcertou() {
+    mgsDeInstrucao.innerHTML = `A letra (${valorDoInput}) existe na palavra`;
+    mgsDeInstrucao.style.color = 'green';
+    revelarCaracteres(palavra, valorDoInput);
+
+}
+
+function jogadorErrou() {
+    mgsDeInstrucao.innerHTML = `A letra (${valorDoInput}) não existe na palavra`;
+    mgsDeInstrucao.style.color = 'orange';
+
+    numeroDeTentativas = numeroDeTentativas - 1;
+    tentativasNohtml.innerHTML = numeroDeTentativas;
+}
+
+function jogadorGanhou() {
+    mgsDeInstrucao.innerHTML = 'Você Ganhou :)';
+    document.getElementById('input-letra').style.display = 'none';
+    document.getElementById('botao-enviar').setAttribute('value', 'Jogar Novamente')
+
+    document.getElementById('botao-enviar').addEventListener('click', () => {
+        window.location.reload();
+    })
+
+    document.getElementById('palavra-no-html').classList.add('active');
+}
+
+function jogadorPerdeu() {
+    mgsDeInstrucao.innerHTML = `Você Perdeu :( a palavra era (${palavra})`;
+    mgsDeInstrucao.style.color = 'red'
+    document.getElementById('input-letra').style.display = 'none';
+    imgForca.setAttribute('src', './img/boneco-enforcado.jpg');
+    document.getElementById('botao-enviar').setAttribute('value', 'Jogar Novamente')
+
+    document.getElementById('botao-enviar').addEventListener('click', () => {
+        window.location.reload();
+    })
+}
+
+function limparCampo() {
+    let limparBotao = document.getElementById('input-letra');
+    limparBotao.value = '';
+}
+
+adicionarAoHtml(tentativasNohtml, numeroDeTentativas);
+adicionarAoHtml(quantidadeDeLetrasNoHtml, numeroDeLetras);
+adicionarAoHtml(tipo, tipoDaPalavra);
+adicionarAoHtml(palavraNoHtml, palavraEscondida);
 
 formulario.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -67,53 +110,27 @@ formulario.addEventListener('submit', (e) => {
         letrasChutada.push(valorDoInput);
 
         if (palavra.includes(valorDoInput)) {
-            mgsDeInstrucao.innerHTML = `A letra (${valorDoInput}) existe na palavra`;
-            mgsDeInstrucao.style.color = 'green';
-            revelarCaracteres(palavra, valorDoInput);
+            jogadorAcertou();
 
             var strDefault = palavraEscondida;
             var str = strDefault.replace(/\s/g, '');
 
-            console.log(str)
-
-            // Verificar se o jogador ganhou
             if (palavra === str) {
-                mgsDeInstrucao.innerHTML = 'Você Ganhou :)';
-                document.getElementById('input-letra').style.display = 'none';
-                document.getElementById('botao-enviar').setAttribute('value', 'Jogar Novamente')
-            
-                document.getElementById('botao-enviar').addEventListener('click', () => {
-                    window.location.reload();
-                })
+                jogadorGanhou();
             }
 
         } else {
-            mgsDeInstrucao.innerHTML = `A letra (${valorDoInput}) não existe na palavra`;
-            mgsDeInstrucao.style.color = 'orange';
-
-            numeroDeTentativas = numeroDeTentativas - 1;
-            tentativasNohtml.innerHTML = numeroDeTentativas;
+            jogadorErrou();
 
             if (numeroDeTentativas == 0) {
-                mgsDeInstrucao.innerHTML = 'Você Perdeu :(';
-                mgsDeInstrucao.style.color = 'red'
-                document.getElementById('input-letra').style.display = 'none';
-                imgForca.setAttribute('src', './img/boneco-enforcado.jpg');
-                document.getElementById('botao-enviar').setAttribute('value', 'Jogar Novamente')
-
-                document.getElementById('botao-enviar').addEventListener('click', () => {
-                    window.location.reload();
-                })
+                jogadorPerdeu();
             }
         }
     } else if (letrasChutada.includes(valorDoInput)) {
-        mgsDeInstrucao.innerHTML = `Você já escolheu a letra ${valorDoInput}, tente outra.`;
+        mgsDeInstrucao.innerHTML = `Você já escolheu a letra (${valorDoInput}), tente outra.`;
+        mgsDeInstrucao.style.color = 'orange';
     }
 
-    let limparBotao = document.getElementById('input-letra');
-    limparBotao.value = '';
+    limparCampo();
 
 });
-
-
-
